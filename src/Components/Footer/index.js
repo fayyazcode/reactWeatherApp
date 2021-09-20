47ec1a2a07fcab9454ad66f3793b6d087814bf10
Fayyaz Alam <fayazalam.98@gmail.com>
2021-09-20 10:40:41 -0700

    header and footer components

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,14 @@
+import './style.css' 
+ function Header() {
+    return (
+        <div className='footer'>
+        <ul>
+            <li>home</li>
+            <li>about</li>
+            <li>Contact</li>
+        </ul>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/Components/Header/index.js
@@ -0,0 +1,10 @@
+import './style.css' 
+ function Header() {
+    return (
+        <div className='header'>
+        <h1>Weather App</h1>
+        </div>
+    )
+}
+
+export default Header;
